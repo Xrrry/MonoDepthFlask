278601eec7cd6654d9d4259c838833eeede82f9e
Xrrry <48988621+Xrrry@users.noreply.github.com>
2021-02-01 15:22:03 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, request, redirect, url_for
+from flask import render_template, send_file, send_from_directory,json, jsonify, make_response
+from test_simple import *
+
+UPLOAD_FOLDER = './save'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+
+# download_model_if_doesnt_exist(args.model_name)
+model_path = os.path.join("models", "mono_640x192")
+print("-> Loading model from ", model_path)
+encoder_path = os.path.join(model_path, "encoder.pth")
+depth_decoder_path = os.path.join(model_path, "depth.pth")
+
+# LOADING PRETRAINED MODEL
+print("   Loading pretrained encoder")
+encoder = networks.ResnetEncoder(18, False)
+loaded_dict_enc = torch.load(encoder_path, map_location=device)
+
+# extract the height and width of image that this model was trained with
+feed_height = loaded_dict_enc['height']
+feed_width = loaded_dict_enc['width']
+filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
+encoder.load_state_dict(filtered_dict_enc)
+encoder.to(device)
+encoder.eval()
+
+print("   Loading pretrained decoder")
+depth_decoder = networks.DepthDecoder(
+    num_ch_enc=encoder.num_ch_enc, scales=range(4))
+
+loaded_dict = torch.load(depth_decoder_path, map_location=device)
+depth_decoder.load_state_dict(loaded_dict)
+
+depth_decoder.to(device)
+depth_decoder.eval()
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/feedback/<s>')
+def feedback(s):
+    return s
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def uploaded_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            test_simple('.\\save\\' + filename, feed_width, feed_height, device, encoder, depth_decoder)
+            return get_file(filename.split('.')[0] + '_disp.jpeg')
+
+@app.route('/get_file/<file_name>', methods=['GET'])
+def get_file(file_name):
+    directory = './save'
+    try:
+        response = make_response(
+            send_from_directory(directory, file_name, as_attachment=True))
+        return response
+    except Exception as e:
+        return jsonify({"code": "异常", "message": "{}".format(e)})
+
+
+if __name__ == '__main__':
+    print("app")
+    app.run()
---
+++ test_simple.py
@@ -0,0 +1,93 @@
+# Copyright Niantic 2019. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the Monodepth2 licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+import glob
+import argparse
+import numpy as np
+import PIL.Image as pil
+import matplotlib as mpl
+import matplotlib.cm as cm
+
+import torch
+from torchvision import transforms, datasets
+
+import networks
+from layers import disp_to_depth
+from utils import download_model_if_doesnt_exist
+
+
+
+def test_simple(ima_path, feed_width, feed_height, device, encoder, depth_decoder):
+    """Function to predict for a single image or folder of images
+    """
+
+    # FINDING INPUT IMAGES
+    if os.path.isfile(ima_path):
+        # Only testing on a single image
+        paths = [ima_path]
+        output_directory = os.path.dirname(ima_path)
+    elif os.path.isdir(ima_path):
+        # Searching folder for images
+        # paths = glob.glob(os.path.join(ima_path, '*.{}'.format(args.ext)))
+        paths = glob.glob(os.path.join(ima_path, '*png'))
+        output_directory = ima_path
+    else:
+        raise Exception("Can not find ima_path: {}".format(ima_path))
+
+    print("-> Predicting on {:d} test images".format(len(paths)))
+
+    # PREDICTING ON EACH IMAGE IN TURN
+    with torch.no_grad():
+        for idx, image_path in enumerate(paths):
+
+            if image_path.endswith("_disp.jpg"):
+                # don't try to predict disparity for a disparity image!
+                continue
+
+            # Load image and preprocess
+            input_image = pil.open(image_path).convert('RGB')
+            original_width, original_height = input_image.size
+            input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
+            input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+
+            # PREDICTION
+            input_image = input_image.to(device)
+            features = encoder(input_image)
+            outputs = depth_decoder(features)
+
+            disp = outputs[("disp", 0)]
+            disp_resized = torch.nn.functional.interpolate(
+                disp, (original_height, original_width), mode="bilinear", align_corners=False)
+
+            # Saving numpy file
+            output_name = os.path.splitext(os.path.basename(image_path))[0]
+            name_dest_npy = os.path.join(output_directory, "{}_disp.npy".format(output_name))
+            scaled_disp, _ = disp_to_depth(disp, 0.1, 100)
+            np.save(name_dest_npy, scaled_disp.cpu().numpy())
+
+            # Saving colormapped depth image
+            disp_resized_np = disp_resized.squeeze().cpu().numpy()
+            vmax = np.percentile(disp_resized_np, 95)
+            normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+            mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+            colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+            im = pil.fromarray(colormapped_im)
+
+            name_dest_im = os.path.join(output_directory, "{}_disp.jpeg".format(output_name))
+            im.save(name_dest_im)
+
+            print("   Processed {:d} of {:d} images - saved prediction to {}".format(
+                idx + 1, len(paths), name_dest_im))
+
+    print('-> Done!')
+
+
+if __name__ == '__main__':
+    print('error_in_main')
